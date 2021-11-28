d7126e95809f84952a763cff125f0e53b73b0663
NiangPro <NiangProgrammeur@gmail.com>
2021-11-28 03:43:54 +0000

    init config

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
     require_once("models/Database.php");
     require_once("views/includes/_header.php");
 
+    //connexion globale a la base de donnees la variable db est globale
+    $db = new Database();
+
     // routeur
     if (isset($_GET['page'])) {
         switch ($_GET['page']) {
--- models/Database.php
+++ models/Database.php
@@ -1,6 +1,6 @@
 <?php
 
-class ClassName
+class Database
 {
     private $db;
     
