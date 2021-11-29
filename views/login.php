be02e5336885f5d01000bb123e16a462958c43d6
NiangPro <NiangProgrammeur@gmail.com>
2021-11-29 17:05:05 +0000

    refractor login page after merging

--- views/login.php
+++ views/login.php
@@ -1,36 +1,4 @@
-<<<<<<< HEAD
-<html>
-    <head>
-       <meta charset="utf-8">
-        <!-- importer le fichier de style -->
-        <link rel="stylesheet" href="style.css" media="screen" type="text/css" />
-    </head>
-    <body>
-        <div id="container">
-            <!-- zone de connexion -->
-            
-            <form action="verification.php" method="POST">
-                <h1>Connexion</h1>
-                
-                <label><b>Nom d'utilisateur</b></label>
-                <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
 
-                <label><b>Mot de passe</b></label>
-                <input type="password" placeholder="Entrer le mot de passe" name="password" required>
-
-                <input type="submit" id='submit' value='LOGIN' >
-                <?php
-                if(isset($_GET['erreur'])){
-                    $err = $_GET['erreur'];
-                    if($err==1 || $err==2)
-                        echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
-                }
-                ?>
-            </form>
-        </div>
-    </body>
-</html>
-=======
 <?php require_once("views/includes/_errors.php") ?>
 <div class="container col-md-3 mt-5 pt-5">
     <div class="card">
@@ -51,5 +19,3 @@
     </div>
     
 </div>
-
->>>>>>> niangpro
