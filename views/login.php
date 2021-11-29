7540d6ca3b7b3121e44bd0486e8740e104f30e93
MamytaPro <mariemesylla97@gmail.com>
2021-11-29 09:55:38 +0000

    Third Ajout on Mamyta

---
+++ views/login.php
@@ -0,0 +1,17 @@
+<div class="wrapper">
+    <h1>
+        Connexion
+    </h1>
+    <p>
+
+    </p>
+    <section cass="login-container" position="fixed" top="0" left="0" bottom="0" background="images/photoBib.jfif">
+        <div width="450px" margin="auto" padding="20px" text-align="center">
+            <form action="" method="post">
+                <input type="text" name="username" placeholder="Nom d'utilisateur" required/>
+                <input type="password" name="password" placeholder="Mot de Passe" required/>
+                <button type="submit">Se Connecter</button>
+            </form>
+        </div>    
+    </section>
+</div>
