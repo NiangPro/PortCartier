c71b3479ea092e6f52b5079e8813cb9eb436e0c9
MamytaPro <mariemesylla97@gmail.com>
2021-11-29 13:35:42 +0000

    Fifth Ajout on Mamyta

--- views/login.php
+++ views/login.php
@@ -5,7 +5,7 @@
     <p>
 
     </p>
-    <section cass="login-container" position="fixed" top="0" left="0" bottom="0" background="images/photoBib.jfif">
+    <section class="login-container" position="fixed" top="0" left="0" bottom="0" background-image="images/bib.">
         <div width="450px" margin="auto" padding="20px" text-align="center">
             <form action="" method="post" display="flex" flex-direction="column">
                 <input type="text" name="username" placeholder="Nom d'utilisateur" required/>
