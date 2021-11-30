53f25746d04d233f93c190ebab2e290a0217297e
MamytaPro <mariemesylla97@gmail.com>
2021-11-30 17:02:15 +0000

    Eleventh Ajout on mamyta

--- views/membre/reservation.php
+++ views/membre/reservation.php
@@ -1,22 +1,24 @@
-<div class="card col-md-5 mt-3 pt-3">
-    <div class="card-body">
-        <b>
-            <h3 class="text-center pb-2">Faire une réservation</h3>
-        </b>
-        <form action="" method="post">
-            <div class="form-group">
-                <label for="">Code Membre</label>
-                <input type="text" name="code" id="" class="form-control" placeholder="Entrez votre code" required>
-            </div>
-            <div class="form-group">
-                <label for="">Code Document</label>
-                <input type="text" name="code" id="" class="form-control" placeholder="Entre le code document" required>
-            </div>
-            <div class="form-group">
-                <label for="">Date Réservation</label>
-                <input type="date" name="datepret" value="">
-            </div>
-                <button name="login" class="btn btn-outline-success btn-sm btn-rounded"> Valider </button>
-        </form>
+<div class="card">
+    <div class="container col-md-5 ">
+        <div class="card-body">
+            <b>
+                <h3 class="text-center pb-2">Faire une réservation</h3>
+            </b>
+            <form action="" method="post">
+                <div class="form-group">
+                    <label for="">Code Membre</label>
+                    <input type="text" name="code" id="" class="form-control" placeholder="Entrez votre code" required>
+                </div>
+                <div class="form-group">
+                    <label for="">Code Document</label>
+                    <input type="text" name="code" id="" class="form-control" placeholder="Entre le code document" required>
+                </div>
+                <div class="form-group">
+                    <label for="">Date Réservation</label>
+                    <input type="date" name="datepret" value="">
+                </div>
+                    <button name="login" class="btn btn-outline-success btn-sm btn-rounded"> Valider </button>
+            </form>
+        </div>
     </div>
 </div>
