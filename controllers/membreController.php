b2f63725ffbb25c7b98e796f0f5451fab390b0d1
NiangPro <NiangProgrammeur@gmail.com>
2021-11-30 13:37:16 +0000

    refractor membrecontroller

--- controllers/membreController.php
+++ controllers/membreController.php
@@ -1,9 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-if($_GET['page'] == "reservation"){
-=======
-
 // Traitement des requetes
 
 
@@ -16,7 +12,7 @@ if($_GET['page'] == "reservation"){
 if($_GET['page'] == "pret membre"){
     require_once("views/membre/pret.php");
 }elseif($_GET['page'] == "reservation membre"){
->>>>>>> niangpro
+
     require_once("views/membre/reservation.php");
 }else{
     require_once("views/membre/document.php");
